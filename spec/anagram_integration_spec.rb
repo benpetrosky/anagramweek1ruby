b1950ba10d208d57aa51e8c5c23155499a0c6b92
Ben Petrosky <benpetrosky@hotmail.com>
2017-04-21 15:46:02 -0700

    add experimental integration

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -6,10 +6,10 @@ set(:show_exceptions, false)
 describe('the anagram path', {:type => :feature}) do
   it('accepts user input and displays result') do
     visit('/')
-    fill_in('first_input', :with => 'random')
-    fill_in('second_input', :with => 'fully')
+    fill_in('first_input', :with => 'mariners')
+    fill_in('second_input', :with => 'mariners')
 
     click_button('Send')
-    expect(page).to have_content("These words have no letter matches and are antigrams.")
+    expect(page).to have_content("You entered the exact same word twice")
   end
 end
